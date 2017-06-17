130f93c4db8a9946f84a478a177a727ddb5b7b77
Radovan <radovanbalog@gmail.com>
2017-06-17 18:39:47 +0200

    Deleted functions file
    Moved into the room object

--- scripts/functions.py
+++
@@ -1,7 +0,0 @@
-from scripts import constants
-
-def checkWall(y, x, room):
-    return room.map[int(y // constants.tileHeight)][int(x // constants.tileWidth)] == 1
-
-def checkMove(y,x,height,width, room):
-    return checkWall(y, x, room) or checkWall(y + height, x, room) or checkWall(y, x + width, room) or checkWall(y + height, x + width, room)
